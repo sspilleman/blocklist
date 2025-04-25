086a549fd0dbe40cc8c6c983f088182d5bf0f5fb
Sander Spilleman <sander@spilleman.nl>
2025-04-25 07:39:33 +0200

    2025-04-25T07-39-33

--- file.ts
+++ file.ts
@@ -1,56 +1,52 @@
-import { blacklist, dynamic } from "./blacklist.ts";
+import { blacklist, dynamic } from "./lists.ts";
 
 const url = "https://filters.adavoid.org/ultimate-ad-filter.txt";
 let txt: string | undefined = undefined;
 let lines: string[] | undefined = undefined;
 
 const regexes: RegExp[] = [
-    /.*xvideos.*/,
-    /^$/,
+  /xvideos/i,
+  /^$/i,
+  /xhcdn/i,
+  /tsyndicate/i,
+  /hpyjmp/i,
+  /blcdog/i,
+  /acscdn/i,
+  /tapecontent\.net/i,
 ];
 
 const filter = (line: string) => {
-    return regexes.filter((r) => r.test(line)).length === 0;
+  const length = regexes.filter((r) => r.test(line)).length;
+  //   if (line.includes("tsyndicate")) console.log(length, line);
+  return length === 0;
 };
 
 const download = async () => {
-    const response = await fetch(url);
-    // age = new Date().getTime();
-    if (response.ok) {
-        console.log("download ok");
-        txt = await response.text();
-        lines = txt.split(/\n/).filter(filter);
-        console.log("lines:", lines.length);
-    } else {
-        console.log("download failed");
-        lines = undefined;
-    }
+  const response = await fetch(url);
+  // age = new Date().getTime();
+  if (response.ok) {
+    console.log("download ok");
+    txt = await response.text();
+    lines = txt.split(/\n/);
+  } else {
+    console.log("download failed");
+    lines = undefined;
+  }
 };
 
 export const get = async (): Promise<string | undefined> => {
-    await download();
-    if (lines) {
-        for (const url of dynamic) {
-            lines.push(url);
-        }
-        for (const url of blacklist.filter((s) => s.endsWith("/"))) {
-            lines.push(`*.${url.slice(0, -1)}`);
-        }
-        for (const url of blacklist.filter((s) => !s.endsWith("/"))) {
-            lines.push(`/${url.replace(/\-/g, "\\-")}/i`);
-        }
-        return lines.join("\n");
+  await download();
+  if (lines) {
+    for (const url of dynamic) {
+      lines.push(url);
     }
-    // const now = new Date().getTime();
-    // if ((now - age) > 1000 * 60 * 60 * 24) {
-    //     console.log("refreshing");
-    //     await download();
-    //     if (lines) return lines.join("\n");
-    //     else return undefined;
-    // } else if (lines) {
-    //     console.log("cached");
-    //     return lines.join("\n");
-    // } else {
-    //     return undefined;
-    // }
+    for (const url of blacklist.filter((s) => s.endsWith("/"))) {
+      lines.push(`*.${url.slice(0, -1)}`);
+    }
+    for (const url of blacklist.filter((s) => !s.endsWith("/"))) {
+      lines.push(`/${url.replace(/\-/g, "\\-")}/i`);
+    }
+    console.log("lines:", lines.filter(filter).length);
+    return lines.filter(filter).join("\n");
+  }
 };
